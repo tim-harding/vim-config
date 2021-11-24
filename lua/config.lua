cd919a5b5669d300acd5b165d833c0c2aeaf2967
Tim Harding <Tim@TimHarding.co>
2021-11-24 12:55:54 -0800

    Moved keybindings to Lua

--- lua/config.lua
+++ lua/config.lua
@@ -1,3 +1,9 @@
+-- Escapes codes such as <C-n> and <Tab>
+function _G.t(string)
+	-- https://neovim.io/doc/user/api.html#nvim_replace_termcodes()
+	return vim.api.nvim_replace_termcodes(string, true, true, true)
+end
+
 local function reload(module_name)
 	package.loaded[module_name] = nil
 	local module = require(module_name)
--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,9 +1,3 @@
--- Escapes codes such as <C-n> and <Tab>
-local function t(string)
-	-- https://neovim.io/doc/user/api.html#nvim_replace_termcodes()
-	return vim.api.nvim_replace_termcodes(string, true, true, true)
-end
-
 local function keybindings()
 	-- Operator-pending would be, for example, when you've typed 
 	-- 'y' before executing 'yw'
@@ -51,63 +45,53 @@ local function keybindings()
 		remap_settings
 	)
 
-	-- " Use normal paste command
-	-- " imap <C-v> <Esc>"+pa
-	-- 
-	-- " " Copy to clipboard
-	-- vnoremap  <leader>y  "+y
-	-- nnoremap  <leader>Y  "+yg_
-	-- nnoremap  <leader>y  "+y
-	-- nnoremap  <leader>yy  "+yy
-	-- 
-	-- " " Paste from clipboard
-	-- nnoremap <leader>p "+p
-	-- nnoremap <leader>P "+P
-	-- vnoremap <leader>p "+p
-	-- vnoremap <leader>P "+P
-	-- 
-	-- " Resizing panes
-	-- nnoremap <silent> <Leader>+ :exe "resize " . (winheight(0) * 3/2)<CR>
-	-- nnoremap <silent> <Leader>- :exe "resize " . (winheight(0) * 2/3)<CR>
-	-- 
-	-- " Moving between panes
-	-- nnoremap <leader><Down> <C-W><C-J>
-	-- nnoremap <leader><Up> <C-W><C-K>
-	-- nnoremap <leader><Left> <C-W><C-H>
-	-- nnoremap <leader><Right> <C-W><C-L>
-	-- 
-	-- " Close braces automatically
-	-- imap {<CR> {<CR>}<Esc>O
-	-- imap [<CR> [<CR>]<Esc>O
-	-- imap (<CR> (<CR>)<Esc>O
-	-- 
-	-- " Fix indentation on selection
-	-- nmap <leader>= gg=G<C-O>
-	-- 
-	-- 
-	-- 
-	-- 
-	-- " ----------------------------------------------------------
-	-- "  LSP
-	-- " ----------------------------------------------------------
-	-- 
-	-- " Code navigation shortcuts
-	-- nnoremap gh     <cmd>lua vim.lsp.buf.hover()<CR>
-	-- nnoremap gi    <cmd>lua vim.lsp.buf.implementation()<CR>
-	-- nnoremap gH 	<cmd>lua vim.lsp.buf.signature_help()<CR>
-	-- nnoremap gr    <cmd>lua vim.lsp.buf.references()<CR>
-	-- nnoremap g0    <cmd>lua vim.lsp.buf.document_symbol()<CR>
-	-- nnoremap gW    <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
-	-- nnoremap gd    <cmd>lua vim.lsp.buf.definition()<CR>
-	-- nnoremap gD   	<cmd>lua vim.lsp.buf.type_definition()<CR>
-	-- nnoremap ga    <cmd>lua vim.lsp.buf.code_action()<CR>
-	-- 
-	-- " Goto previous/next diagnostic warning/error
-	-- nnoremap <silent> g[ <cmd>lua vim.lsp.diagnostic.goto_prev()<CR>
-	-- nnoremap <silent> g] <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
-	-- 
-	-- " Show diagnostic popup on cursor hold
-	-- autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()
+	-- Grow pane size
+	vim.api.nvim_set_keymap(
+		Mode.normal,
+		"<Leader>+",
+		":exe \"resize \" . (winheight(0) * 3/2)<CR>",
+		remap_settings
+	)
+
+	-- Shrink pane size
+	vim.api.nvim_set_keymap(
+		Mode.normal,
+		"<Leader>-",
+		":exe \"resize \" . (winheight(0) * 2/3)<CR>",
+		remap_settings
+	)
+
+	-- Move pane down
+	vim.api.nvim_set_keymap(
+		Mode.normal,
+		"<Leader><Down>",
+		"<C-W><C-J>",
+		remap_settings
+	)
+
+	-- Move pane up
+	vim.api.nvim_set_keymap(
+		Mode.normal,
+		"<Leader><Up>",
+		"<C-W><C-K>",
+		remap_settings
+	)
+
+	-- Move pane left
+	vim.api.nvim_set_keymap(
+		Mode.normal,
+		"<Leader><Left>",
+		"<C-W><C-H>",
+		remap_settings
+	)
+
+	-- Move pane right
+	vim.api.nvim_set_keymap(
+		Mode.normal,
+		"<Leader><Right>",
+		"<C-W><C-L>",
+		remap_settings
+	)
 end
 
 return keybindings
